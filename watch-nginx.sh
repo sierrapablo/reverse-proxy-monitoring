b3713462755393737c002d32ec676671978cdf37
sierrapablo <pablosierralorente@outlook.es>
2025-06-13 14:37:42 +0200

    Refactor watcher script

--- watch-nginx.sh
+++ watch-nginx.sh
@@ -3,22 +3,28 @@
 echo "[INFO] Watching for changes in /etc/nginx/conf.d (.conf only)"
 
 inotifywait -m -e close_write,create,delete,moved_to,moved_from --format '%e %f' /etc/nginx/conf.d |
-while read event file; do
-    if echo "$file" | grep -qE '^\..*|~$|\.sw[px]*$|4913$'; then
+while read -r event file; do
+
+    echo "$file" | grep -qE '^\..*|~$|\.sw[px]*$|4913$'
+    if [ $? -eq 0 ]; then
         echo "[DEBUG] Ignored temporary file: $file"
         continue
     fi
 
-    if echo "$file" | grep -q '\.conf$'; then
-        echo "[INFO] Relevant event detected: $event $file"
-        if nginx -t; then
-            echo "[INFO] Reloading nginx"
-            nginx -s reload
-        else
-            echo "[ERROR] nginx config is invalid, reload aborted"
-        fi
-    else
+    echo "$file" | grep -q '\.conf$'
+    if [ $? -ne 0 ]; then
         echo "[DEBUG] Non-conf file ignored: $file"
+        continue
+    fi
+
+    echo "[INFO] Relevant event detected: $event $file"
+
+    nginx -t
+    if [ $? -eq 0 ]; then
+        echo "[INFO] Reloading nginx"
+        nginx -s reload
+    else
+        echo "[ERROR] nginx config is invalid, reload aborted"
     fi
 done
 
